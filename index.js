577624ee8f57eb27e93654e35db6ba7d82c572fa
Rida Fatima <162724587+ridafatima05@users.noreply.github.com>
2024-05-09 19:27:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node 
+//shabang
+import inquirer from "inquirer";
+// random num
+// input
+// correct or not
+// result
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    { name: "userGuessingNumber",
+        type: "number",
+        message: "Please Guess a Number between 1-6:",
+    }
+]);
+if (answer.userGuessingNumber === randomNumber) {
+    console.log("Congratulations! you guessed the right  number.");
+}
+else {
+    console.log("You guessed the wrong number.");
+}
